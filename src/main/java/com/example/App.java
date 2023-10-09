21dc3968b3e25a86af0070a693f80acf16d355d5
bernatScolastico <lai2-01@itismeucci.com>
2023-10-09 10:38:04 +0200

    finito server

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -1,13 +1,39 @@
 package com.example;
 
-/**
- * Hello world!
- *
- */
+import java.io.BufferedReader;
+import java.io.DataOutputStream;
+import java.io.InputStreamReader;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+
 public class App 
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        try {
+            System.out.println("Server Avviato");
+            ServerSocket server = new ServerSocket(3000);
+
+            Socket s = server.accept();
+            System.out.println("Un cliente si è connesso");
+
+            BufferedReader in = new BufferedReader(new InputStreamReader(s.getInputStream()));
+            DataOutputStream out =  new DataOutputStream(s.getOutputStream());
+
+            String stringaRicevuta = in.readLine();
+            System.out.println("Il client ha inviato " + stringaRicevuta);
+
+            String stringaModificata = stringaRicevuta.toUpperCase();
+            out.writeBytes(stringaModificata + "\n");
+
+            server.close();
+            s.close();
+
+        } catch (Exception e) {
+            System.out.println("Qualcosa è andato storto");
+            System.out.println(e.getMessage());
+            System.exit(1);
+        }
     }
 }
